21f261306d34ee16f08184c289730bbdca26e897
Alexm0u <alexm0u.13@gmail.com>
2023-01-30 13:16:06 +0100

    ejercicio basico

---
+++ main.js
@@ -0,0 +1,69 @@
+console.log("hola");
+
+//comentario de una linea
+
+/*
+comentario
+de
+varias
+lineas*/
+
+// var variable1 = true;
+// var variable2 = 'soy la variable 2';
+// var variable = 35;
+
+// console.log(variable1)
+// console.log(variable2)
+// console.log(variable3)
+
+// let variableLet1 = 'soy la variable let 1'
+
+// console.log(variableLet1)
+
+// const variableConstUno = 23
+
+// console.log(variableConstUno)
+
+// let numeroUno = 23
+// let numeroDos = 32
+
+// let suma = numeroUno + numeroDos
+
+// console.log(suma)
+
+// let resta = numeroDos - numeroUno
+// console.log(resta)
+
+// let incrementarUno = ++numeroUno
+
+// console.log(incrementarUno)
+
+// let decrecerUno = --numeroUno
+
+// console.log(decrecerUno)
+
+let = numero;
+
+if (numero <= 4) {
+  ("Insuficiente");
+} else if (5 || 6) {
+  ("suficiente");
+} else if (7 || 8) {
+  ("notable");
+} else if (9 || 10) {
+  ("sobresaliente");
+}
+
+// resuelto:
+
+let nota = prompt("Introduce la nota del alumno");
+
+if (nota >= 0 && nota <= 4) {
+  console.log("Insuficiente");
+} else if (nota == 5 || nota == 6) {
+  console.log("suficiente");
+} else if (nota == 7 || nota == 8) {
+  console.log("notable");
+} else if (nota == 9 || nota == 10) {
+  console.log("Sobresaliente");
+}
